cba072b63d010a55901fdeec4c87ad9d6c6381ac
Vansh Jaiswal <vanshjaiswal31@gmail.com>
2024-08-23 00:10:22 +0530

    Add files via upload

---
+++ Recursions/Subsequence_Pattern/combination_sum_1.py
@@ -0,0 +1,35 @@
+class Recursion:
+
+    def combination_sum(self, index, arr, target, intermediate, output):
+        if index >= len(arr):
+            return
+        if target==0:
+            output.append(intermediate.copy())
+       
+        if arr[index] <= target:
+            intermediate.append(arr[index])
+            print(intermediate, target)
+            self.combination_sum(index, arr, target-arr[index], intermediate, output)
+            intermediate.pop()
+            target +=arr[index]
+        self.combination_sum(index+1, arr, target-arr[index], intermediate, output)
+
+        return output
+
+
+
+    def driver(self, arr, target):
+        output=[]
+        intermediate = []
+        index = 0
+        if arr==[]:
+            return output
+        self.combination_sum(index, arr, target, intermediate, output)
+        return output
+
+if __name__ == "__main__":
+    solution = Recursion()
+    arr=[8,7,4,3]
+    target=11
+    ans = solution.driver(sorted(arr), target)
+    print(ans)
